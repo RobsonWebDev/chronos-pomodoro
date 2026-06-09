00470aec86bb63639a46fd6f8a9f1bf9b80b7875
Robson Cerqueira <robsonwebpro@gmail.com>
2026-06-09 15:26:16 -0300

    Configurando ciclos

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,13 +2,61 @@ import { PlayCircleIcon } from "lucide-react";
 import { Cycles } from "../Cycles";
 import { DefaultButton } from "../DefaultButton";
 import { DefaultInput } from "../DefaultInput";
+import { useRef } from "react";
+import type { TaskModel } from "../../models/TaskModel";
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
+import { getNextCycle } from "../../utils/getNextCycle";
 
 
 export function MainForm() {
+    const { state, setState } = useTaskContext()
+    const taskNameInput = useRef<HTMLInputElement>(null)
+
+    const nextCycle = getNextCycle(state.currentCycle)
+
+    console.log(nextCycle)
+
+    function handleCreateNewtask(event: React.SubmitEvent<HTMLFormElement>) {
+        event.preventDefault()
+
+        if(taskNameInput.current === null) return;
+
+        const taskName = taskNameInput.current.value.trim();
+
+        if(!taskName){
+            alert('Digite o nome da tarefa')
+            return
+        }
+
+        const newTask: TaskModel = {
+            id: Date.now().toString(),
+            name: taskName,
+            startDate: Date.now(),
+            completeDate: null,
+            interruptDate: null,
+            duration: 1,
+            type: 'workTime'
+        };
+
+        const secondsRemaining = newTask.duration * 60
+
+        setState(prevState => {
+            return {
+                ...prevState,
+                config: {...prevState.config},
+                activeTask: newTask,
+                currentCycle: nextCycle,
+                secondsRemaining,
+                formattedSecondsRemaining: '',
+                tasks: [...prevState.tasks, newTask],
+            }
+        })
+    }
+
     return (
-        <form className='form' action="">
+        <form onSubmit={handleCreateNewtask} className='form' action="">
             <div className="formRow">
-                <DefaultInput id='meuInput' labelText='task' type='text' placeholder='Digite' />
+                <DefaultInput id='meuInput' labelText='task' type='text' placeholder='Digite' ref={taskNameInput}/>
             </div>
 
             <div className="formRow">
